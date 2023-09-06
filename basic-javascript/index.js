3233cdc7838fa69f7a775298aa6e14b6d2c6773c
deveripon <developershahadathussainripon@gmail.com>
2023-09-06 12:50:58 +0600

    Updated : upto functions Chapter

--- basic-javascript/index.js
+++ basic-javascript/index.js
@@ -1,3 +1,33 @@
-//Javascript Comments
-//JavaScript comments can be used to explain JavaScript code, and to make it more readable.JavaScript comments can also be used to prevent execution, when testing alternative code.
-// example: 
+//example of var
+//================================================================
+// var title = "Javascript"; //declaration of a variable using var
+// console.log(title); //output : javascript
+// title = "python"; //We can change the value of the variable
+// console.log(title); //output : python
+// var title = "Ripon Miah"; //We can redeclare same variable in var
+// console.log(title); //output : Ripon miah
+//================================================================
+
+// //example of let
+// //================================================================
+// let title = "Bangladesh"; //declaration of a variable
+// console.log(title); //output :Bangladesh
+// title = "python"; //we can change the value of the variable
+// console.log(title); //output :python
+// // let title = "Ripon Miah"; //we can not redeclare same variable using let
+// console.log(title); // it give you an error
+// // /media/devripon/devripon/practice folder/javascript-documentation/basic-javascript/index.js:17^
+// // SyntaxError: Identifier 'title' has already been declared
+// //    at internalCompileFunction (node:internal/vm:73:18)
+//================================================================
+
+// //example of const
+// const title = "Javascript"; //decalare a variable using const
+// console.log(title); //output : "Javascript
+// title = "python"; //you can't change the value of this variable
+// console.log(title); // it give you an error
+// /media/devripon/devripon/practice folder/javascript-documentation/basic-javascript/index.js:27
+// title = "python"; //you can't change the value of this variable
+//       ^
+// TypeError: Assignment to constant variable.
+//     at Object.<anonymous> (/media/devripon/devripon/practice folder/javascript-documentation/basic-javascript/index.js:27:7)

950097cf0f3a5a732a7ed870a42513aec96e93dc
deveripon <developershahadathussainripon@gmail.com>
2023-09-06 10:39:57 +0600

    initialize: javascript documentation

---
+++ basic-javascript/index.js
@@ -0,0 +1,3 @@
+//Javascript Comments
+//JavaScript comments can be used to explain JavaScript code, and to make it more readable.JavaScript comments can also be used to prevent execution, when testing alternative code.
+// example: 
